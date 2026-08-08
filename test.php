96db95b2708535a8a27e7687b882a07ed930e5e7
NARAYANAN2024 <gouth2000@yahoo.com>
2026-08-08 23:52:26 +0530

    Add files via upload

---
+++ test.php
@@ -0,0 +1,53 @@
+
+<html>
+<head>
+<meta charset="utf-8">
+
+<title> Learing CSS</title>
+<link rel="stylesheet" href="css/style1.css">
+</head>
+<body>
+<div id="header">
+<header>
+<div id="he1">
+<h1>VISHNU THE UNIVERSE</h1>
+</div>
+<div id="he2">
+<img src="images/image1.jfif" style="width:417px;height:150px">
+</div>
+</header>
+</div>
+<div id="nav">
+<center>
+<ul id="ut">
+<li><a href="#">HOME</a></li>
+<li><a href="#">DESCRIPTION</a></li>
+<li><a href="#">CONTACT</a></li>
+<li><a href="#">SIGNIN</a></li>
+</ul>
+</center>
+</div>
+<div id="container">
+<div id="con1"></div>
+<div id="con2">
+
+<form action="register.php" method="POST">
+<div class="mb-3">
+<input type="text" class="form-control"name="name" placeholder="Enter Your Name">
+</div>
+<div class="mb-3">
+<input type="text" class="form-control"name="address" placeholder="Enter Your Address">
+</div>
+<div class="mb3">
+<input type="text" class="form-control" name="mobile" placeholder="Enter Your Mobile Number">
+</div>
+<div class="mb-3">
+<input type="submit" name="submit" value="submit">
+</div>
+</form>
+</div>
+</div>
+
+</body>
+</html>
+
